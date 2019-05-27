aa79bf9696cc829791cf5589baa914da35838188
Vladislav Solonina <lissvskiss@gmail.com>
2019-05-27 11:00:32 +0300

    Main

---
+++ Test3/Test3/Pokemon.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Text;
+using Test3.Annotations;
+using Xamarin.Forms;
+
+namespace Test3
+{
+    public class Pokemon : INotifyPropertyChanged
+    {
+        public string name;
+        public string pokemonName;
+        public int height;
+        public int weight;
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (name != value)
+                {
+                    name = value;
+                    OnPropertyChanged(nameof(Name));
+                }
+            }
+        }
+        public string PokemonName
+        {
+            get { return pokemonName; }
+            set
+            {
+                if (pokemonName != value)
+                {
+                    pokemonName = value;
+                    OnPropertyChanged("PokemonName");
+                }
+            }
+        }
+
+        public int Height
+        {
+            get { return height; }
+            set
+            {
+                if (height != value)
+                {
+                    height = value;
+                    OnPropertyChanged("Height");
+                }
+            }
+        }
+        public int Weight
+        {
+            get { return weight; }
+            set
+            {
+                if (weight != value)
+                {
+                    weight = value;
+                    OnPropertyChanged("Weight");
+                }
+            }
+        }
+        public ImageSource ImageSource { get; set; }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            //PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ Test3/Test3/PokemonPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Test3
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PokemonPage : ContentPage
+    {
+        private Pokemon pokemon;
+
+        public PokemonPage(Pokemon pokemon)
+        {
+            InitializeComponent();
+            this.pokemon = pokemon;
+            var bindingName = new Binding
+            {
+                Source = pokemon,
+                Path = "Name"
+            };
+
+            Title = pokemon.PokemonName;
+            Name.SetBinding(Label.TextProperty, bindingName);
+
+            Height.Text = pokemon.Height.ToString();
+            Weight.Text = pokemon.Weight.ToString();
+
+        }
+
+        private async void Changing(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ChangePokemonPage(pokemon));
+        }
+    }
+}
